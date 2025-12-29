d665183e0b05e755647d135704b20ae7dbf577fc
Oli-Dom <oadl12345678@gmail.com>
2025-12-28 22:00:14 -0500

    got rid of boilerplate | added router dependency | simple landing

---
+++ src/components/Hero.vue
@@ -0,0 +1,18 @@
+<template>
+<div id="hero-container">
+ <h1>Welcome to Lift Tracker</h1>
+ <br>
+  <h2>Get 1RM predictions, organized tracking, and visual analytics all in one place</h2>
+</div>
+ 
+</template>
+
+<style scoped>
+#hero-container{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+}
+
+</style>
